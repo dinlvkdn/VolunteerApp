5ce5502f2d47fa8bf28c50eebdbc393c7a1f8ec8
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-11 15:24:19 +0300

    Add new DTO CreateJobOfferDTO

---
+++ Domain/DTOs/CreateJobOfferDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.DTOs
+{
+    public class CreateJobOfferDTO
+    {
+        public string JobOfferTitle { get; set; }
+        public string JobOfferStreet { get; set; }
+        public string JobOfferCity { get; set; }
+        public string JobOfferCountry { get; set; }
+        public string Description { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
