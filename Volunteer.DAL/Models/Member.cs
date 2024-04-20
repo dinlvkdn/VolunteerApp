60b616a27e2146a0518ad0a51fbc97ca60b8f34a
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-20 20:27:13 +0300

    Made the FeedbackId value null

--- Volunteer.DAL/Models/Member.cs
+++ Volunteer.DAL/Models/Member.cs
@@ -6,12 +6,10 @@ namespace Volunteer.DAL.Models
         public Guid VolunteerId { get; set; }
         public Volunteer Volunteer { get; set; }
 
-        public Guid FeedbackId { get; set; }
-        public Feedback Feedback { get; set; }
+        public Guid? FeedbackId { get; set; }
+        public Feedback? Feedback { get; set; }
 
         public Guid JobOfferId { get; set; }
         public JobOffer JobOffer { get; set; }
-
-
     }
 }
