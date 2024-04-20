cb2412afa02f88ba328ba5c6cfcc6a5535b7e057
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-04-20 20:24:52 +0300

    Added the Rating, OrganizationId, VolunteerId fields to the Feedback model

--- Volunteer.DAL/Models/Feedback.cs
+++ Volunteer.DAL/Models/Feedback.cs
@@ -4,7 +4,9 @@ namespace Volunteer.DAL.Models
     public class Feedback : BaseEntity
     {
         public string Description { get; set; }
-
+        public int Rating { get; set; }
+        public Guid OrganizationId { get; set; }
+        public Guid VolunteerId { get; set; }
 
         public Member Member { get; set; }
     }
